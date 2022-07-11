35259b3f3b8777a1a113eed539296d75a918a8bd
Luis Luna <luiscarloslunach@gmail.com>
2022-07-11 13:00:15 -0500

    microservicios

---
+++ user-service/src/main/java/com/tutorial/userservice/entity/User.java
@@ -0,0 +1,21 @@
+package com.tutorial.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+}
